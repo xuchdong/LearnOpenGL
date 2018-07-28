604fe7ccc0a7484567f5e80c9673e37f4b2ceda1
xuchdong <xuchdong@gmail.com>
2018-07-28 16:23:45 +0800

    删除 Shader shader_m cpp 文件

--- LearnOpenGL/Shader.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Shader.cpp
-//  LearnOpenGL
-//
-//  Created by xuchdong on 2018/7/21.
-//  Copyright © 2018年 com.xuchdong. All rights reserved.
-//
-
-#include "Shader.h"
--- LearnOpenGL/shader_m.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  shader_m.cpp
-//  LearnOpenGL
-//
-//  Created by xuchdong on 2018/7/28.
-//  Copyright © 2018年 com.xuchdong. All rights reserved.
-//
-
-#include "shader_m.hpp"
