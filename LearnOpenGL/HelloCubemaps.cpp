12d0798c34fea72d8a70c6aa547718ddc9cc44c5
xuchdong <xuchdong@gmail.com>
2018-08-02 22:51:54 +0800

    fix：书写错误不能正常显示

--- LearnOpenGL/HelloCubemaps.cpp
+++ LearnOpenGL/HelloCubemaps.cpp
@@ -118,9 +118,9 @@ void init(GLFWwindow *window)
 
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), skyboxVertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
     glBindVertexArray(0);
 
     cubemapTexture = loadCubemap(faces);
