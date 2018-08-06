802360ab16e46fcf4c7bdea83a54b7da704350b9
xuchdong <xuchdong@gmail.com>
2018-08-06 23:18:52 +0800

    添加 planeVAO, planeVBO

--- LearnOpenGL/HelloShadowMapping.cpp
+++ LearnOpenGL/HelloShadowMapping.cpp
@@ -11,15 +11,55 @@
 
 #ifdef HELLO_SHADOWMAPPING
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+unsigned int planeVAO, planeVBO;
+
 void init(GLFWwindow* window)
 {
-    
+    glEnable(GL_DEPTH_TEST);
+
+    float planeVertices[] = {
+         25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,  25.0f,  0.0f,
+        -25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,   0.0f,  0.0f,
+        -25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,   0.0f, 25.0f,
+
+         25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,  25.0f,  0.0f,
+        -25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,   0.0f, 25.0f,
+         25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,  25.0f, 10.0f
+    };
+
+    glGenVertexArrays(1, &planeVAO);
+    glBindVertexArray(planeVAO);
+    glGenBuffers(1, &planeVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), planeVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(0 + 3 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(0 + 6 * sizeof(float)));
+    glBindVertexArray(0);
 }
 
 void draw()
 {
-    
+    /*
+    glClear(GL_DEPTH_BUFFER_BIT);
+
+    glm::vec3 lightPos(-2.0f, 4.0f, -1.0f);
+    glm::mat4 lightProjection, lightView;
+    glm::mat4 lightSpaceMatrix;
+    float near_plane = 1.0f, far_plane = 7.5f;
+    lightProjection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, near_plane, far_plane);
+    lightView = glm::lookAt(lightPos, glm::vec3(0.0f), glm::vec3(0.0, 1.0, 0.0));
+    lightSpaceMatrix = lightProjection * lightView;
+    */
 }
+
 void clean()
 {
     
