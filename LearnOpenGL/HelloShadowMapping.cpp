6f4f018716ef4df30d69d845a961c412735f6d82
xuchdong <xuchdong@gmail.com>
2018-08-07 22:26:34 +0800

    添加loadTexture 函数

--- LearnOpenGL/HelloShadowMapping.cpp
+++ LearnOpenGL/HelloShadowMapping.cpp
@@ -11,15 +11,23 @@
 
 #ifdef HELLO_SHADOWMAPPING
 
+#include <iostream>
+
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
 #include "shader_m.h"
+#include "stb_image.h"
 
+using namespace std;
+
+GLuint loadTexture(const char* path);
 void renderScene(const Shader &shader);
 void renderCube();
 void renderQuad();
+
+string srcBasepath = "/Users/xuchdong/xuchdong/LearnOpenGL/src/";
 
 unsigned int planeVAO, planeVBO;
 
@@ -69,6 +77,46 @@ void draw()
 void clean()
 {
     
+}
+
+GLuint loadTexture(const char* path)
+{
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+
+    int width, height, nrComponents;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char* data = stbi_load(path, &width, &height, &nrComponents, 0);
+    if(data)
+    {
+        GLenum format;
+        if(nrComponents == 1)
+        {
+            format = GL_RED;
+        }
+        else if(nrComponents == 3)
+        {
+            format = GL_RGB;
+        }
+        else if(nrComponents == 4)
+        {
+            format = GL_RGBA;
+        }
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    }
+    else
+    {
+        cout << "Texture failed to load at path: " << path << endl;
+    }
+    stbi_image_free(data);
+    return textureID;
 }
 
 void renderScene(const Shader &shader)
