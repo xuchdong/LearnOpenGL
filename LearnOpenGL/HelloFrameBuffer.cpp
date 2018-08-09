b0f7dd38e9d63146f8b334e6233bbf9f470a34bc
xuchdong <xuchdong@gmail.com>
2018-08-09 09:08:56 +0800

    添加纹理部分

--- LearnOpenGL/HelloFrameBuffer.cpp
+++ LearnOpenGL/HelloFrameBuffer.cpp
@@ -12,9 +12,59 @@
 
 #ifdef HELLO_FRAMEBUFFER
 
+#include <iostream>
+#include <string>
+
+#include "stb_image.h"
+
+using namespace std;
+
+string srcBasepath = "/Users/xuchdong/xuchdong/LearnOpenGL/LearnOpenGL/src/";
+
 unsigned int cubeVAO, cubeVBO;
 unsigned int planVAO, planVBO;
 unsigned int quadVAO, quadVBO;
+unsigned int cubeTexture, floorTexture;
+
+unsigned int loadTexture(const char* path)
+{
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+
+    int width, height, nrComponents;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char* data = stbi_load(path, &width, &height, &nrComponents, 0);
+    if(data)
+    {
+        GLenum format;
+        if(nrComponents == 1)
+        {
+            format = GL_RED;
+        }
+        else if(nrComponents == 3)
+        {
+            format = GL_RGB;
+        }
+        else if(nrComponents == 4)
+        {
+            format = GL_RGBA;
+        }
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    }
+    else
+    {
+        cout << "Texture failed to load at path: " << path << endl;
+    }
+    stbi_image_free(data);
+    return textureID;
+}
 
 void init(GLFWwindow* window)
 {
@@ -115,6 +165,9 @@ void init(GLFWwindow* window)
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)(2 * sizeof(float)));
     glBindVertexArray(0);
+
+    cubeTexture = loadTexture((srcBasepath + "marble.jpg").c_str());
+    floorTexture = loadTexture((srcBasepath + "metal.png").c_str());
 }
 
 void draw()
