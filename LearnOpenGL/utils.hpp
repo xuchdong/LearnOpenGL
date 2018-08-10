eca885114b1783571a0221c846d6b7a3a6d025e0
xuchdong <xuchdong@gmail.com>
2018-08-10 23:09:54 +0800

    将 loadTexture 抽出来放到 utils.hpp文件中

---
+++ LearnOpenGL/utils.hpp
@@ -0,0 +1,61 @@
+//
+//  utils.hpp
+//  LearnOpenGL
+//
+//  Created by xuchdong on 2018/8/10.
+//  Copyright © 2018年 com.xuchdong. All rights reserved.
+//
+
+#ifndef utils_hpp
+#define utils_hpp
+
+#include <stdio.h>
+#include <iostream>
+
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+unsigned int loadTexture(const char* path)
+{
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+
+    int width, height, nrComponents;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char* data = stbi_load(path, &width, &height, &nrComponents, 0);
+    if(data)
+    {
+        GLenum format;
+        if(nrComponents == 1)
+        {
+            format = GL_RED;
+        }
+        else if(nrComponents == 3)
+        {
+            format = GL_RGB;
+        }
+        else if(nrComponents == 4)
+        {
+            format = GL_RGBA;
+        }
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    }
+    else
+    {
+        cout << "Texture failed to load at path: " << path << endl;
+    }
+    stbi_image_free(data);
+    return textureID;
+}
+
+#endif /* utils_hpp */
