4783dd24a33e573b2d164c55c853b7b5170a3a2d
xuchdong <xuchdong@gmail.com>
2018-08-10 09:27:04 +0800

    添加使用framebuffer

--- LearnOpenGL/HelloFrameBuffer.cpp
+++ LearnOpenGL/HelloFrameBuffer.cpp
@@ -15,8 +15,13 @@
 #include <iostream>
 #include <string>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include "stb_image.h"
 #include "shader_m.h"
+#include "camera.h"
 
 using namespace std;
 
@@ -26,8 +31,11 @@ unsigned int cubeVAO, cubeVBO;
 unsigned int planVAO, planVBO;
 unsigned int quadVAO, quadVBO;
 unsigned int cubeTexture, floorTexture;
+unsigned int framebuffer, textureColorBuffer, rbo;
 Shader *shader;
 Shader *screenShader;
+
+Camera camera(glm::vec3(0.0f, 0.0f, 4.0f));
 
 unsigned int loadTexture(const char* path)
 {
@@ -180,11 +188,71 @@ void init(GLFWwindow* window)
 
     screenShader->use();
     screenShader->setInt("screenTexture", 0);
+
+    glGenFramebuffers(1, &framebuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+
+    glGenTextures(1, &textureColorBuffer);
+    glBindTexture(GL_TEXTURE_2D, textureColorBuffer);
+    glTexImage2D(GL_TEXTURE_2D, 0 , GL_RGB, SRC_WIDTH, SRC_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorBuffer, 0);
+
+    glGenRenderbuffers(1, &rbo);
+    glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, SRC_WIDTH, SRC_HEIGHT);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
+    if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+    {
+        cout << "ERROR: FRAMEBUFFER: Framebuffer is not complete!" << endl;
+    }
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
 
 void draw()
 {
 
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+    glEnable(GL_DEPTH_TEST);
+    glClear(GL_DEPTH_BUFFER_BIT);
+
+    shader->use();
+    glm::mat4 model = glm::mat4(1.0f);
+    glm::mat4 view = camera.GetViewMatrix();
+    glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float) SRC_WIDTH / (float) SRC_HEIGHT, 0.1f, 100.0f);
+    shader->setMat4("view", view);
+    shader->setMat4("projection", projection);
+
+    glBindVertexArray(cubeVAO);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, cubeTexture);
+    model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
+    shader->setMat4("model", model);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+
+    model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
+    shader->setMat4("model", model);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+
+    glBindVertexArray(planVAO);
+    model = glm::mat4(1.0f);
+    glBindTexture(GL_TEXTURE_2D, floorTexture);
+    shader->setMat4("model", model);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    glBindVertexArray(0);
+
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    glDisable(GL_DEPTH_TEST);
+    
+   glClearColor(1.0f, 1.0f, 1.0f, 1.0f); 
+   glClear(GL_COLOR_BUFFER_BIT);
+
+   screenShader->use();
+   glBindVertexArray(quadVAO);
+   glBindTexture(GL_TEXTURE_2D, textureColorBuffer);
+   glDrawArrays(GL_TRIANGLES, 0, 6);
 }
 
 void clean()
@@ -195,5 +263,6 @@ void clean()
     glDeleteBuffers(1, &cubeVBO);
     glDeleteBuffers(1, &planVBO);
     glDeleteBuffers(1, &quadVBO);
+    glDeleteFramebuffers(1, &framebuffer);
 }
 #endif
