813244863cbe056267eeaa8b828f616fb0acd3ee
xuchdong <xuchdong@gmail.com>
2018-08-13 09:20:39 +0800

    配置 float framebuffer

--- LearnOpenGL/HelloBloom.cpp
+++ LearnOpenGL/HelloBloom.cpp
@@ -11,8 +11,56 @@
 
 #ifdef HELLO_BLOOM
 
+#include "shader_m.h"
+#include "utils.hpp"
+
+Shader *shader, *shaderLight, *shaderBlur, *shaderBloomFinal;
+unsigned int woodTexture, containerTexture;
+
+unsigned int hdrFBO;
+unsigned int colorBuffers[2];
+unsigned int rboDepth;
+
 void init(GLFWwindow* window)
 {
+    glEnable(GL_DEPTH_TEST);
+
+    shader = new Shader("bloom.vs", "bloom.fs");
+    shaderLight = new Shader("bloom.vs", "light_box.fs");
+    shaderBlur = new Shader("blur.vs", "blur.fs");
+    shaderBloomFinal = new Shader("bloom_final.vs", "bloom_final.fs");
+
+    woodTexture = loadTexture("wood.png", true);
+    containerTexture = loadTexture("container2.png", true);
+
+    glGenFramebuffers(1, &hdrFBO);
+    glBindFramebuffer(GL_FRAMEBUFFER, hdrFBO);
+
+    glGenTextures(2, colorBuffers);
+    for(unsigned int i = 0; i < 2; i++)
+    {
+        glBindTexture(GL_TEXTURE_2D, colorBuffers[i]);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB16F, SRC_WIDTH, SRC_HEIGHT, 0, GL_RGB, GL_FLOAT, NULL);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        // attach texture to framebuffer
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + i, GL_TEXTURE_2D, colorBuffers[i], 0);
+    }
+
+    glGenRenderbuffers(1, &rboDepth);
+    glBindRenderbuffer(GL_RENDERBUFFER, rboDepth);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, SRC_WIDTH, SRC_HEIGHT);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, rboDepth);
+
+    unsigned int attachments[2] = {GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1};
+    glDrawBuffers(2, attachments);
+    if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+    {
+        cout << "Framebuffer not complete!" << endl;
+    }
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
 
 void draw()
