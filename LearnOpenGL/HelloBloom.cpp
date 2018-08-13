77f58a07f22848bae975025952291fbd4004817d
xuchdong <xuchdong@gmail.com>
2018-08-13 08:55:13 +0800

    初始化 bloom 环境

---
+++ LearnOpenGL/HelloBloom.cpp
@@ -0,0 +1,26 @@
+//
+//  HelloBloom.cpp
+//  LearnOpenGL
+//
+//  Created by xuchdong on 2018/8/13.
+//  Copyright © 2018年 com.xuchdong. All rights reserved.
+//
+
+#include <stdio.h>
+#include "LearnOpenGL.h"
+
+#ifdef HELLO_BLOOM
+
+void init(GLFWwindow* window)
+{
+}
+
+void draw()
+{
+}
+
+void clean()
+{
+}
+
+#endif
--- LearnOpenGL/LearnOpenGL.h
+++ LearnOpenGL/LearnOpenGL.h
@@ -13,7 +13,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#define HELLO_HDR
+#define HELLO_BLOOM
 
 #define SRC_WIDTH   800
 #define SRC_HEIGHT  600
