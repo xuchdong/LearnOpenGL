9221a7ae994c44f773544d5e580303999eaaba5b
xuchdong <xuchdong@gmail.com>
2018-08-13 09:36:41 +0800

    添加光照的 framebuffer

--- LearnOpenGL/HelloBloom.cpp
+++ LearnOpenGL/HelloBloom.cpp
@@ -21,6 +21,9 @@ unsigned int hdrFBO;
 unsigned int colorBuffers[2];
 unsigned int rboDepth;
 
+unsigned int pingpongFBO[2];
+unsigned int pingpongColorbuffers[2];
+
 void init(GLFWwindow* window)
 {
     glEnable(GL_DEPTH_TEST);
@@ -61,6 +64,24 @@ void init(GLFWwindow* window)
         cout << "Framebuffer not complete!" << endl;
     }
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    glGenFramebuffers(2, pingpongFBO);
+    glGenTextures(2, pingpongColorbuffers);
+    for(unsigned int i = 0; i < 2; i++)
+    {
+        glBindFramebuffer(GL_FRAMEBUFFER, pingpongFBO[i]);
+        glBindTexture(GL_TEXTURE_2D, pingpongColorbuffers[i]);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB16F, SRC_WIDTH, SRC_HEIGHT, 0, GL_RGB, GL_FLOAT, NULL);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, pingpongColorbuffers[i], 0);
+        if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        {
+            cout << "Framebuffer not complete!" << endl;
+        }
+    }
 }
 
 void draw()
