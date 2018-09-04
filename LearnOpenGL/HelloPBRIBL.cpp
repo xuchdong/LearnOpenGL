db9c4d1021d2a44f429d2b9def0d00158104b96b
xuchdong <xuchdong@gmail.com>
2018-09-04 23:30:07 +0800

    初始化 PBR IBL 环境

---
+++ LearnOpenGL/HelloPBRIBL.cpp
@@ -0,0 +1,32 @@
+//
+//  HelloPBRIBL.cpp
+//  LearnOpenGL
+//
+//  Created by xuchdong on 2018/9/4.
+//  Copyright © 2018年 com.xuchdong. All rights reserved.
+//
+
+#include <stdio.h>
+#include "LearnOpenGL.h"
+#ifdef HELLO_PBR_IBL
+
+#define SRC_WIDTH   800
+#define SRC_HEIGHT  600
+
+GLFWwindow* mainWin;
+
+void init(GLFWwindow* window)
+{
+    mainWin = window;
+}
+
+void draw()
+{
+
+}
+
+void clean()
+{
+
+}
+#endif
